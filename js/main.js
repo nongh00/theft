f182b8efec3ea33d6aa6a54d639bcfe65ba3a917
nongh00 <100324943+nongh00@users.noreply.github.com>
2022-02-24 03:45:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+ $('.desing-slider').slick({
+ 	dots:false,
+ 	slidesToShow: 2	,
+  	variableWidth: true,
+  	prevArrow:'<img src="images/Arrow-l.svg" class="arrow arrow-left">' ,
+  	nextArrow:'<img src="images/Arrow-r.svg" class="arrow arrow-right">' ,
+ });
+});
